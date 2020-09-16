d4c8890d08810262d83aaef8870b3800deae4282
see-bet-code <brittani.elan.taylor@gmail.com>
2020-09-15 22:02:34 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    count = 0
+    self.strip.split.map { |e| count += 1 if e.sentence? || e.question? \
+      || e.exclamation?}
+    count
   end
 end
